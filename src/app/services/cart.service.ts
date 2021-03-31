7f12a71cb0e662ad32bbf9521f465805fc9b60bb
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-03-31 15:03:55 +0300

    .

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,9 +11,9 @@ export class CartService {
   constructor() { }
   addToCart(product:Product){
     let item=CartItems.find(c=>c.product.productId==product.productId);
-    if(item){
+   if(item){
       item.quantity+=1;
-    }
+     }
     else{
       let cartItem=new CartItem();
       cartItem.product=product;
@@ -21,14 +21,14 @@ export class CartService {
       CartItems.push(cartItem)
 
     }
-  }
+   }
   removeFromCart(product:Product){
-    let item:CartItem=CartItems.find(c=>c.product.productId==product.productId);
+    let item:CartItem=CartItems.find(c=>c.product.productId == product.productId);
     CartItems.splice(CartItems.indexOf(item),1);
 
   }
-  list():CartItem[]{
-    return CartItems;
+  //  list():CartItem[]{
+  //    return CartItems;
 
-  }
+  // }
 }
